3c7f7ccbd5a52816d099ec10886aef70ec90c511
Hasan Nagiev <hasannagiev@gmail.com>
2025-09-06 06:10:22 +0400

    postgres_destekli

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
+# Render PostgreSQL üçün DATABASE_URL mühit dəyişəni istifadə edilir
+#DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://fastapi_testdb_user:32F78QYjVnThi5AhKCYWbirlaNuOnbTR@localhost:5432/fastapi_testdb")
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-# Render PostgreSQL üçün DATABASE_URL mühit dəyişəni istifadə edilir
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://fastapi_testdb_user:32F78QYjVnThi5AhKCYWbirlaNuOnbTR@localhost:5432/fastapi_testdb")
+# Render-da environment variable istifadə et
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
