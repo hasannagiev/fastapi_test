a0a25cfd5a6e2a3d49c5e33aecd73d65b0100b34
Hasan Nagiev <hasannagiev@gmail.com>
2025-09-06 06:22:27 +0400

    postgres_destekli2

--- app/database.py
+++ app/database.py
@@ -6,9 +6,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Render-da environment variable istifadə et
 DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set!")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
